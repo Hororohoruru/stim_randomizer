0068043dc2480e630af0eb9efd7018ac33a8e83d
Juan Jesus Torre <juanjesustorre@gmail.com>
2018-12-07 17:07:39 +0100

    added test cases for subsets

--- tests/test_prerandomizations.py
+++ tests/test_prerandomizations.py
@@ -18,7 +18,7 @@ from stim_randomizer.prerandomizations import create_prerandomizations, pseudo_l
 from stim_randomizer.subsets import create_stim_sets
 
 # Define the categories
-categories = ["human", "animal", "nature"]
+categories = ("human", "animal", "nature")
 
 
 def cleanup_files(*args):
@@ -35,6 +35,46 @@ def cleanup_files(*args):
         shutil.rmtree(path)
 
 
+def set_up_subsets(file_number, set_number, cat_list):
+    """Perform setup for tests that need subsets as an input
+
+    Parameters
+    ----------
+
+    file_number: int
+                 total number of files from which subsets will be created
+
+    set_number: int
+                number of subsets desired
+
+    cat_list: tuple
+              categories that will be used to create the files
+
+    Returns
+    -------
+
+    subset_folder: str
+                   path which contains the .tsv files for the subsets
+
+    output_folder: str
+                   path for the output of the test functions
+    """
+
+    temp_input_folder = tempfile.mkdtemp()
+    subset_folder = tempfile.mkdtemp()
+    output_folder = tempfile.mkdtemp()
+
+    for category in cat_list:
+        for _ in range(file_number // len(cat_list)):
+            tempfile.mkstemp(prefix=(category + '%02d' % _), dir=temp_input_folder)
+
+    create_stim_sets(temp_input_folder, set_number, cat_list, output_path=subset_folder)
+
+    shutil.rmtree(temp_input_folder)
+
+    return subset_folder, output_folder
+
+
 def set_up_files(file_number, cat=False, categories=None):
     """Performs the necessary setup for test functions that take files as input
 
@@ -42,13 +82,13 @@ def set_up_files(file_number, cat=False, categories=None):
     ----------
 
     file_number: int
-                 number of file that will be created as input. If cat is False, this will be the total number of
+                 number of files that will be created as input. If cat is False, this will be the total number of
                  files. If it is set to True, this will be the number of files per category
 
     cat: bool, default False
          if True, it will append the category names provided in categories to the temp files
 
-    categories: list, default None
+    categories: tuple, default None
                 names of the categories to be used. Note that it will only be taken into account if cat=True
 
     Returns
@@ -66,7 +106,7 @@ def set_up_files(file_number, cat=False, categories=None):
 
     if cat:
         for category in categories:
-            for i in range(file_number // len(categories)):
+            for i in range(file_number):
                 tempfile.mkstemp(prefix=(category + '%02d' % i), dir=input_folder)
 
     else:
@@ -126,41 +166,34 @@ def test_pseudo_label_mapper():
 def test_subset_parser():
 
     # Variables
-    files_per_category = 48
+    file_number = 144
     number_of_sets = 4
-    files_per_set = files_per_category * len(categories) / number_of_sets
+    files_per_set = file_number // number_of_sets
 
-    # Set up the subsets using create_stim_sets from subsets.py
-    temp_input_folder = tempfile.mkdtemp()
+    subset_folder, _ = set_up_subsets(file_number, number_of_sets, categories)
 
-    for category in categories:
-        for _ in range(files_per_category):
-            tempfile.mkstemp(prefix=(category + '%02d' % _), dir=temp_input_folder)
-
-    temp_output_folder = tempfile.mkdtemp()
-
-    create_stim_sets(temp_input_folder, number_of_sets, categories, output_path=temp_output_folder)
-
-    parsed_sets = subset_parser(temp_output_folder)
+    parsed_sets = subset_parser(subset_folder)
 
     # Test
 
-    assert len(parsed_sets) == len(os.listdir(temp_output_folder))
+    assert len(parsed_sets) == len(os.listdir(subset_folder))
 
     for subset in parsed_sets:
 
         assert len(subset) == files_per_set
 
-    cleanup_files(temp_input_folder, temp_output_folder)
+    cleanup_files(subset_folder, _)
 
 
 def test_create_prerandomizations():
 
-    testing_parameters = {'file_number': [80, 90, 90],
-                          'files_per_category': [0, 30, 30],
-                          'prerandom_number': [3, 3, 3],
-                          'constrained': [False, True, True],
-                          'method': ['pseudo', 'pseudo', 'pure']}
+    testing_parameters = {'file_number': [80, 90, 90, 144],
+                          'files_per_category': [0, 30, 30, 12],
+                          'prerandom_number': [3, 3, 3, 3],
+                          'constrained': [False, True, True, False],
+                          'subsets': [False, False, False, True],
+                          'subset_number': [1, 1, 1, 4],
+                          'method': ['pseudo', 'pseudo', 'pure', 'pseudo']}
 
     # From testing_parameters, create a list of dictionaries with the same keys, but only one of the values,
     # to use for the different test cases
@@ -172,30 +205,42 @@ def test_create_prerandomizations():
         # TEST CASE 1: 3 prerandomizations, no categories, no subsets
         # TEST CASE 2: 3 prerandomizations, 3 categories, no subsets, pseudo method
         # TEST CASE 3: 3 prerandomizations, 3 categories, no subsets, pure method
+        # TEST CASE 4: 3 prerandomizations, 3 categories, 4 subsets, pseudo method
 
-        if case['files_per_category'] != 0:
-            temp_input_folder, temp_output_folder = set_up_files(case["file_number"], cat=True, categories=categories)
+        if case['subsets']:
+            temp_input_folder, temp_output_folder = set_up_subsets(case['file_number'],
+                                                                   case['subset_number'],
+                                                                   categories)
+
+        elif case['files_per_category'] != 0:
+            temp_input_folder, temp_output_folder = set_up_files(case['files_per_category'],
+                                                                 cat=True,
+                                                                 categories=categories)
 
         else:
-            temp_input_folder, temp_output_folder = set_up_files(case["file_number"])
+            temp_input_folder, temp_output_folder = set_up_files(case['file_number'])
 
         # Run the function and test
         create_prerandomizations(temp_input_folder,
                                  case['prerandom_number'],
                                  temp_output_folder,
                                  categories=categories,
+                                 subsets=case['subsets'],
                                  constrained=case['constrained'],
                                  method=case['method'])
 
         prerands = os.listdir(temp_output_folder)
 
-        assert len(prerands) == case['prerandom_number']
+        assert len(prerands) == case['prerandom_number'] * case['subset_number']
 
         for file in prerands:
             path = os.path.join(temp_output_folder, file)
             prerand_df = pd.read_table(path, header=None)
 
-            assert len(prerand_df) == case['file_number']
+            if case['subsets']:
+                assert len(prerand_df) == case['file_number'] // case['subset_number']
+            else:
+                assert len(prerand_df) == case['file_number']
 
         # Delete temp folders and files
         cleanup_files(temp_input_folder, temp_output_folder)
