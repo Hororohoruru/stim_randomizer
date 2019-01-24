b19feeb44d7d7116fde179a2cc6b324ddfe03eb9
Juan Jesus Torre <juanjesustorre@gmail.com>
2019-01-24 15:53:10 +0100

    moved fixtures to conftest.py

--- tests/test_ExpStim.py
+++ tests/test_ExpStim.py
@@ -23,43 +23,6 @@ from stim_randomizer.ExpStim import ExpStim
 categories = ['animal', 'human', 'nature']
 
 
-@pytest.fixture()
-def setup_cat_dir():
-    test_dir = tempfile.mkdtemp()
-
-    for category in categories:
-        for i in range(10):
-            tempfile.mkstemp(prefix=(category + '_%02d' % i), dir=test_dir)
-
-    return test_dir
-
-
-@pytest.fixture()
-def setup_plain_dir():
-    test_dir = tempfile.mkdtemp()
-
-    for i in range(10):
-        tempfile.mkstemp(dir=test_dir)
-
-    return test_dir
-
-
-@pytest.fixture()
-def setup_cat(setup_cat_dir):
-
-    test_obj = ExpStim(str(setup_cat_dir), categories)
-
-    return test_obj
-
-
-@pytest.fixture()
-def setup_plain(setup_plain_dir):
-
-    test_obj = ExpStim(str(setup_plain_dir))
-
-    return test_obj
-
-
 def test_instantiation_categories(setup_cat):
     es = setup_cat
 
